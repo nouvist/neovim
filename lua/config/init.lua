41db81899b45f5cc1f55aba3e6152e4d88270489
nouvist <13176878+nouvist@users.noreply.github.com>
2022-09-17 17:22:18 +0700

    feat: packer, lspconfig, nvim-cmp, easymotion, and more

---
+++ lua/config/init.lua
@@ -0,0 +1,54 @@
+require('config.plugins')
+require('mason').setup()
+local completion = require('config.completion')
+
+vim.o.background = "dark"
+vim.cmd([[colorscheme gruvbox]])
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.wo.numberwidth = 4
+
+local TAB_WIDTH = 2
+vim.opt.tabstop = TAB_WIDTH
+vim.opt.shiftwidth = TAB_WIDTH
+vim.opt.expandtab = true
+
+local opts = { remap = false, silent = true }
+local opts_remap = { remap = true, silent = true }
+
+vim.keymap.set('n', '<c-h>', ':wincmd h<cr>', opts)
+vim.keymap.set('n', '<c-j>', ':wincmd j<cr>', opts)
+vim.keymap.set('n', '<c-k>', ':wincmd k<cr>', opts)
+vim.keymap.set('n', '<c-l>', ':wincmd l<cr>', opts)
+vim.keymap.set('i', '<c-c>', '<c-[>', opts_remap)
+
+vim.keymap.set('n', 'go', 'g%', opts_remap)
+
+
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<c-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('i', '<c-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+end
+
+local server_list = { 'sumneko_lua', 'tsserver' }
+completion.setup(server_list, on_attach)
---
+++ lua/config/plugins.lua
@@ -0,0 +1,32 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  -- color scheme
+  use 'ellisonleao/gruvbox.nvim'
+
+  -- typing enhance
+  use 'easymotion/vim-easymotion'
+  -- use 'rstacruz/vim-closer'
+  use { 'andymass/vim-matchup', event = 'VimEnter' }
+  use 'tpope/vim-commentary'
+  use 'tpope/vim-surround'
+  use 'vim-scripts/ReplaceWithRegister'
+
+  -- lsp
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/cmp-nvim-lua'
+  use 'hrsh7th/nvim-cmp'
+
+  use 'hrsh7th/cmp-vsnip'
+  use 'hrsh7th/vim-vsnip'
+
+  use "williamboman/mason.nvim"
+end)
