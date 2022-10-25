eee1b27f24c2dfd4bb1867870365db3fd782429e
Ra <108156134+rainrisa@users.noreply.github.com>
2022-10-26 05:06:20 +0700

    fix: deprecated cmp.update_capabilities

--- lua/config/completion.lua
+++ lua/config/completion.lua
@@ -62,7 +62,7 @@ cmp.setup.filetype('lua', {
 })
 
 -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 
 local M = {}
